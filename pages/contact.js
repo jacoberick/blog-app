21e8f3a542f0b78abafaf38433cd9adb67f7eff6
Jacob White <jacobewhitedev@gmail.com>
2021-01-30 00:28:10 -0600

    contact page added

--- pages/contact.js
+++ pages/contact.js
@@ -5,9 +5,14 @@ const Contact = () => {
   return (
     <div className="h-screen">
       <Header />
-      <div id="ContactSection" className="h-96">
-        <h1>FOR ALL INQUIRIES</h1>
-        <p>gulaganthem@gmail.com</p>
+      <div id="ContactSection" className="h-96 flex items-center justify-center flex-col py-72 my-10 text-jet">
+        <h1 className="font-header text-2xl mb-10">FOR ALL INQUIRIES</h1>
+        <a
+          className="font-bookTitle text-7xl hover:text-red transition duration-175"
+          href="mailto:gulaganthem@gmail.com"
+        >
+          GULAGANTHEM@GMAIL.COM
+        </a>
       </div>
       <Footer />
     </div>
